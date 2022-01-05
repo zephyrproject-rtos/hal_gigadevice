fc57324fa4112fa03ee154a34f1bcc44b5845c51
HaiLong Yang <cameledyang@pm.me>
2022-01-05 15:15:27 +0800

    i2c: move clock frequency definition to header
    i2c-gd32 driver needs to know the clock frequency boundary.
    This move the clock frequency definition to i2c header.
    Also add fast mode and fast mode plus minimum clock frequency.
    
    Signed-off-by: HaiLong Yang <cameledyang@pm.me>

--- gd32f3x0/standard_peripheral/include/gd32f3x0_i2c.h
+++ gd32f3x0/standard_peripheral/include/gd32f3x0_i2c.h
@@ -279,6 +279,12 @@ typedef enum
 #define I2C_ADDFORMAT_7BITS           ((uint32_t)0x00000000U)                  /*!< address:7 bits */
 #define I2C_ADDFORMAT_10BITS          I2C_SADDR0_ADDFORMAT                     /*!< address:10 bits */
 
+/* I2C clock frequency, MHz */
+#define I2CCLK_MAX                    ((uint32_t)0x0000003FU)                  /*!< i2cclk maximum value */
+#define I2CCLK_MIN                    ((uint32_t)0x00000002U)                  /*!< i2cclk minimum value for standard mode */
+#define I2CCLK_FM_MIN                 ((uint32_t)0x00000008U)                  /*!< i2cclk minimum value for fast mode */
+#define I2CCLK_FM_PLUS_MIN            ((uint32_t)0x00000018U)                  /*!< i2cclk minimum value for fast mode plus */
+
 /* function declarations */
 /* reset I2C */
 void i2c_deinit(uint32_t i2c_periph);
