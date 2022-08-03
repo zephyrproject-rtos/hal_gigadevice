6c0f8b4e4ff0593926920e41c6e8cb594e002000
Gerard Marull-Paretas <gerard@teslabs.com>
2022-08-03 17:18:36 +0200

    common_include: add GD32E50X support
    Update common_include headers (using gd32headers.py)
    
    Signed-off-by: Gerard Marull-Paretas <gerard@teslabs.com>

--- common_include/gd32_bkp.h
+++ common_include/gd32_bkp.h
@@ -6,6 +6,8 @@
 
 #if defined(CONFIG_SOC_SERIES_GD32E10X)
 #include <gd32e10x_bkp.h>
+#elif defined(CONFIG_SOC_SERIES_GD32E50X)
+#include <gd32e50x_bkp.h>
 #elif defined(CONFIG_SOC_SERIES_GD32F403)
 #include <gd32f403_bkp.h>
 #elif defined(CONFIG_SOC_SERIES_GD32VF103)
