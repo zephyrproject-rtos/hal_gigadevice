dd0e0322474462b58059e6fedaf1d67d2a0864d0
YuLong Yao <feilongphone@gmail.com>
2022-08-20 11:01:31 +0800

    gd32e10x: apply patches for hal
    apply fllowing patches for hal:
     - drop I2CCLK_MAX/I2CCLK_MIN
     - add `gd32_` prefix for timer_init
      - add ifdef to BIT macro
      - remove nvic_vector_table_set function call
    
    Signed-off-by: YuLong Yao <feilongphone@gmail.com>

--- gd32e10x/cmsis/gd/gd32e10x/source/system_gd32e10x.c
+++ gd32e10x/cmsis/gd/gd32e10x/source/system_gd32e10x.c
@@ -145,11 +145,6 @@ void SystemInit (void)
     /* configure the system clock source, PLL Multiplier, AHB/APBx prescalers and Flash settings */
     system_clock_config();
     
-#ifdef VECT_TAB_SRAM
-  nvic_vector_table_set(NVIC_VECTTAB_RAM,VECT_TAB_OFFSET);
-#else
-  nvic_vector_table_set(NVIC_VECTTAB_FLASH,VECT_TAB_OFFSET);
-#endif
 
 }
 
